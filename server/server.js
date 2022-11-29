1decef824a140dca479d97fadfec6c1e1054dd37
FOR-TIMI <timiplayskeys@gmail.com>
2022-11-29 09:54:47 -0500

    added schema and  typrDefs files

--- server/server.js
+++ server/server.js
@@ -3,8 +3,25 @@ const path = require('path');
 const db = require('./config/connection');
 const routes = require('./routes');
 
+
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+//Import apollo server
+const { ApolloServer} = require('apollo-server-express');
+
+const { authMiddleware } = require('./utils/auth');
+
+//import typrDefs and resolvers
+const {typeDefs, resolvers} = require('./schemas');
+
+// create a new Apollo server and pass in our schema data
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: authMiddleware
+})
+
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -16,6 +33,21 @@ if (process.env.NODE_ENV === 'production') {
 
 app.use(routes);
 
-db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
-});
+const startApolloServer = async(typeDefs, resolvers) => {
+   await server.start();
+   //Integrate our apollo server with the express application as middleware
+   server.applyMiddleware({ app })
+
+   db.once('open', () => {
+    app.listen(PORT, () => {
+      console.log(`🌍 Now listening on localhost:${PORT}`)
+      
+      // log where we can go to test our GQL API
+      console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+    });
+  });
+}
+
+
+// Function call to start server
+startApolloServer(typeDefs, resolvers);
